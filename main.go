3120a64ccfeb41b7ec382955d1297c02b20486f3
Fu Zhang <contact@fzhang.co.uk>
2018-11-13 00:48:15 +0000

    init commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+type Member struct {
+	Name             string
+	Gender           string
+	Course           string
+	faculty          string
+	AttendedWorkshop int16
+	Data             int16
+}
+
+func main() {
+
+}
