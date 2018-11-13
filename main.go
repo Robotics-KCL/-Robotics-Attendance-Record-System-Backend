d833286e6252a7d28daa6215c1509ab6056d9e9e
Fu Zhang <contact@fzhang.co.uk>
2018-11-13 01:22:49 +0000

    added data storage

--- main.go
+++ main.go
@@ -11,11 +11,20 @@ type Member struct {
 	Name             string
 	Gender           string
 	Course           string
-	faculty          sFtring
+	faculty          string
 	AttendedWorkshop int16
 	Data             int16
 }
 
 func main() {
 
+}
+
+func Writein(jsonString []byte) {
+
+	var jsonText = []Member
+
+	if err:= json.Unmarshal([]byte(jsonString),&jsonText); err !=nil{
+		log.Println(err)
+	}
 }
