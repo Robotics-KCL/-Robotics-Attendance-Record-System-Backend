92af769ff34001a151b5a0c27d1e322d5b20911a
Fu Zhang <contact@fzhang.co.uk>
2018-11-13 04:09:43 +0000

    Implemented the json writing task

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 )
 
 // for record every entry in the database
@@ -11,20 +13,48 @@ type Member struct {
 	Name             string
 	Gender           string
 	Course           string
-	faculty          string
+	Faculty          string
 	AttendedWorkshop int16
-	Data             int16
+	Date             int32
 }
 
 func main() {
+	fmt.Println("Starting working")
+	Writein(Member{Name: "Alex Bonin", Gender: "Male", Course: "Electonics", Faculty: "NMS", AttendedWorkshop: 10, Date: 13112018})
 
 }
 
-func Writein(jsonString []byte) {
+func Writein(newMember Member) {
 
-	var jsonText = []Member
+	/*
+	 * code for opening a json file from storage
+	 */
+	jsonFile, err := os.Open("members.json")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	if err:= json.Unmarshal([]byte(jsonString),&jsonText); err !=nil{
+	fmt.Println("Successfully Opened users.json")
+	defer jsonFile.Close()
+
+	//read all data in the json file through ioutil
+	existingData, _ := ioutil.ReadAll(jsonFile)
+
+	var mem []Member
+
+	if err := json.Unmarshal([]byte(existingData), &mem); err != nil {
 		log.Println(err)
 	}
+
+	mem = append(mem, newMember)
+
+	newJson, err := json.Marshal(mem)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(string(newJson))
+
+	ioutil.WriteFile("members.json", newJson, 0644)
+
 }
